748e448b229ac4de624750b7598b319c31e997e1
Limpid <Limpid8818@outlook.com>
2025-05-29 16:13:43 +0800

    add ListPagerTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.6.1")
     // RecyclerView支持
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     // 如果使用Material Design组件（可选）
