d1f2a3580a0053803545a378e96e68c9c118d23f
Limpid <Limpid8818@outlook.com>
2025-06-05 20:15:45 +0800

    add some tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:4.11.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.1")
     testImplementation("org.robolectric:robolectric:4.10")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -67,6 +68,7 @@ dependencies {
     implementation("org.json:json:20231013")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")// 必须使用4.x版本
     implementation("org.json:json:20231013")// Android默认库可能版本过低
+    testImplementation(kotlin("test"))
 
 
 }
